e4f1def2364fd8a0076a5d23094c1945534d52bf
Loïc Horellou <hello@loichorellou.net>
2015-10-25 22:05:08 +0100

    add express.io inside PHP
    test nodeJS + php

--- index.php
+++ index.php
@@ -13,11 +13,28 @@ $spine = json_decode(file_get_contents('spine.json'));
 <head>
 	<title>Haunted by algorithms</title>
 	<script src="js/jquery-1.11.3.min.js"></script>
+	<script src="http://localhost:7076/socket.io/socket.io.js"></script>
+	<!--<script src="http://192.168.0.1:167863/socket.io/socket.io.js"></script>-->
+
+
+	<style>
+	*{font-family: monospace; }
+	</style>
+
 </head>
 <body>
 
+	<form action="">
+<p><input type="text" name="reponse_field" id="reponse_field"></p>
+<p><input type="submit" value="envoyer" id="reponse_send"></p>
+</form>
+
+
+<div id="discussion"></div>
+
 	<div id="loader">Chargement du contenu…</div>
 
+	<p>ok</p>
 	<ul id="menu">
 		<?php //print_r($spine); ?>
 	
@@ -66,6 +83,34 @@ $spine = json_decode(file_get_contents('spine.json'));
 				});
 
 			});
+
+
+	io = io.connect();
+
+	// Emit ready event.
+	io.emit('ready');
+
+
+	message("ready");
+	
+
+	// Listen for the talk event.
+	io.on('talk', function(data) {
+	    console.log(data.message);
+
+	    message(data.message, "bot");
+	});
+
+
+	$("#reponse_send").click(function(event){
+
+		event.preventDefault();
+
+		message( $("#reponse_field").val(), "user" );
+		io.emit('message', { texte: $("#reponse_field").val()} );
+
+		$("#reponse_field").val("");
+	})
 
 		})
 
